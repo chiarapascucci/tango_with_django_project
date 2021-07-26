098abfe8d52c51586755104cbe371c074a3cae65
Chiara Pascucci <ch.pascucci@gmail.com>
2021-07-26 16:44:29 +0100

    adding clickable links to about page and back

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,8 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
+    
+    path('about', views.about, name='about'),
     path('',views.index,name='index'),
+    
 ]
--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index (request):
-    return HttpResponse("rango say howdy partner?")
+    return HttpResponse("Rango says hey there partner! go to about: <a href = 'http://127.0.0.1:8000/rango/about' > about </a>")
+
+def about (request):
+    return HttpResponse("Rango says this is the about page <a href= 'http://127.0.0.1:8000/rango' > back </a> ")
