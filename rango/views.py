e572b9c3d8e9c788f7b70878e3fb20b77893c855
Chiara Pascucci <ch.pascucci@gmail.com>
2021-07-27 15:04:04 +0100

    committing exercises for ch 4

--- rango/views.py
+++ rango/views.py
@@ -7,4 +7,6 @@ def index (request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about (request):
-    return HttpResponse("Rango says here is the about page." + "<a href= '/rango/' >Index</a>")
+    context_dict = {'boldmessage':'this tutorial has been put together by Chiara'}
+
+    return render(request, 'rango/about.html', context=context_dict)
