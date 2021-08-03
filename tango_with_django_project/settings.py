d9252cd88f7c189c5629c9c0290e24bae841bb76
Chiara Pascucci <ch.pascucci@gmail.com>
2021-08-03 12:02:53 +0100

    chapter 11 and half of 12

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -30,7 +30,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+REGISTRATION_OPEN = True
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_URL = 'auth_login'
+LOGIN_REDIRECT_URL = 'rango:index'
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -125,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_URL = 'rango:login'
+
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
